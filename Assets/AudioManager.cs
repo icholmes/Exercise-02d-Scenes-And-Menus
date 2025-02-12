61ae3a7f205e6555c1485bad056f1b7e57beee6b
Izzy Holmes <icholmes@iu.edu>
2025-02-12 14:40:23 -0500

    completes the project

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -4,14 +4,28 @@ public class AudioManager : MonoBehaviour
 {
     [Header("-------- Audio Source --------")]
     [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
 
     [Header("-------- Audio Clip --------")]
     public AudioClip background;
+    public AudioClip shooting;
+    public AudioClip explosion;
 
     private void Start()
     {
         musicSource.clip = background;
         musicSource.Play();
     }
-}
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+
+    }
+    
+     
+
+    
+
+}  
 
